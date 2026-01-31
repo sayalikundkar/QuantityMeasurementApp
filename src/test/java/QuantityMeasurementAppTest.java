95c6e54eb4faa35944bbd7b2a97124570e1b902f
sayalikundkar <sayalikundkar28@gmail.com>
2026-01-31 20:29:11 +0530

    Updated QuantityMeasurementApp and test cases

--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -1,6 +1,5 @@
 import org.example.QuantityMeasurementApp;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 public class QuantityMeasurementAppTest {
     @Test
@@ -8,32 +7,28 @@ void testFeetEquality_SameValue() {
     QuantityMeasurementApp.Feet a = new QuantityMeasurementApp.Feet(1.0);
     QuantityMeasurementApp.Feet b = new QuantityMeasurementApp.Feet(1.0);
     assertEquals(a, b);
-    assertEquals(b, a); // symmetry
+    assertEquals(b, a);
 }
-
     @Test
     void testFeetEquality_DifferentValue() {
         QuantityMeasurementApp.Feet a = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet b = new QuantityMeasurementApp.Feet(2.0);
         assertNotEquals(a, b);
     }
-
     @Test
     void testFeetEquality_NullComparison() {
         QuantityMeasurementApp.Feet a = new QuantityMeasurementApp.Feet(1.0);
         assertNotEquals(a, null);
     }
-
     @Test
     void testFeetEquality_DifferentClass() {
         QuantityMeasurementApp.Feet a = new QuantityMeasurementApp.Feet(1.0);
         Object other = new Object();
         assertNotEquals(a, other);
     }
-
     @Test
     void testFeetEquality_SameReference() {
         QuantityMeasurementApp.Feet a = new QuantityMeasurementApp.Feet(1.0);
-        assertEquals(a, a); // reflexive
+        assertEquals(a, a);
     }
 }
